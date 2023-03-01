48380ff9536e5d50b26c2f5b7121745cdc78da56
okormorupraise <okormorupraisecoding@gmail.com>
2023-03-01 02:16:22 -1200

    my-portfolio

--- Myportfolio/pages/api/hello.js
+++ Myportfolio/pages/api/hello.js
@@ -1,5 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export default async function handler(req, res) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate('/')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
 }
